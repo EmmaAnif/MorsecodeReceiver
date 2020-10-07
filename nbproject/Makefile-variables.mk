9a2a9711409d1fd6a748c7947f1e521fd4ba1412
ecarryer <carryer@stanford.edu>
2020-10-06 20:17:04 -0700

    First commit of the PIC32 Framework with the u8g2 library

---
+++ nbproject/Makefile-variables.mk
@@ -0,0 +1,27 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=Framework4PIC32_with_u8g2.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/Framework4PIC32_with_u8g2.production.hex
+CND_PACKAGE_DIR_default=${CND_DISTDIR}/default/package
+CND_PACKAGE_NAME_default=framework4pic32withu8g2.tar
+CND_PACKAGE_PATH_default=${CND_DISTDIR}/default/package/framework4pic32withu8g2.tar
+# Justu8g2POC configuration
+CND_ARTIFACT_DIR_Justu8g2POC=dist/Justu8g2POC/production
+CND_ARTIFACT_NAME_Justu8g2POC=Framework4PIC32_with_u8g2.production.hex
+CND_ARTIFACT_PATH_Justu8g2POC=dist/Justu8g2POC/production/Framework4PIC32_with_u8g2.production.hex
+CND_PACKAGE_DIR_Justu8g2POC=${CND_DISTDIR}/Justu8g2POC/package
+CND_PACKAGE_NAME_Justu8g2POC=framework4pic32withu8g2.tar
+CND_PACKAGE_PATH_Justu8g2POC=${CND_DISTDIR}/Justu8g2POC/package/framework4pic32withu8g2.tar
+# FrameWorkWith_u8g2 configuration
+CND_ARTIFACT_DIR_FrameWorkWith_u8g2=dist/FrameWorkWith_u8g2/production
+CND_ARTIFACT_NAME_FrameWorkWith_u8g2=Framework4PIC32_with_u8g2.production.hex
+CND_ARTIFACT_PATH_FrameWorkWith_u8g2=dist/FrameWorkWith_u8g2/production/Framework4PIC32_with_u8g2.production.hex
+CND_PACKAGE_DIR_FrameWorkWith_u8g2=${CND_DISTDIR}/FrameWorkWith_u8g2/package
+CND_PACKAGE_NAME_FrameWorkWith_u8g2=framework4pic32withu8g2.tar
+CND_PACKAGE_PATH_FrameWorkWith_u8g2=${CND_DISTDIR}/FrameWorkWith_u8g2/package/framework4pic32withu8g2.tar

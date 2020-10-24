bc14daa5c67b444bfa07846878b3fa76da832d1a
Emmanuel Anifowose <anifs@stanford.edu>
2020-10-24 07:21:06 -0700

    edited Project Source folder to include MorseElements SM

--- nbproject/Makefile-variables.mk
+++ nbproject/Makefile-variables.mk
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #
 # Generated - do not edit!
 #
@@ -7,37 +6,15 @@
 CND_BASEDIR=`pwd`
 # Justu8g2POC configuration
 CND_ARTIFACT_DIR_Justu8g2POC=dist/Justu8g2POC/production
-CND_ARTIFACT_NAME_Justu8g2POC=Framework4PIC32_with_u8g2.production.hex
-CND_ARTIFACT_PATH_Justu8g2POC=dist/Justu8g2POC/production/Framework4PIC32_with_u8g2.production.hex
+CND_ARTIFACT_NAME_Justu8g2POC=lab4.production.hex
+CND_ARTIFACT_PATH_Justu8g2POC=dist/Justu8g2POC/production/lab4.production.hex
 CND_PACKAGE_DIR_Justu8g2POC=${CND_DISTDIR}/Justu8g2POC/package
-CND_PACKAGE_NAME_Justu8g2POC=framework4pic32withu8g2.tar
-CND_PACKAGE_PATH_Justu8g2POC=${CND_DISTDIR}/Justu8g2POC/package/framework4pic32withu8g2.tar
+CND_PACKAGE_NAME_Justu8g2POC=lab4.tar
+CND_PACKAGE_PATH_Justu8g2POC=${CND_DISTDIR}/Justu8g2POC/package/lab4.tar
 # FrameworkWith_u8g2 configuration
 CND_ARTIFACT_DIR_FrameworkWith_u8g2=dist/FrameworkWith_u8g2/production
-CND_ARTIFACT_NAME_FrameworkWith_u8g2=Framework4PIC32_with_u8g2.production.hex
-CND_ARTIFACT_PATH_FrameworkWith_u8g2=dist/FrameworkWith_u8g2/production/Framework4PIC32_with_u8g2.production.hex
+CND_ARTIFACT_NAME_FrameworkWith_u8g2=lab4.production.hex
+CND_ARTIFACT_PATH_FrameworkWith_u8g2=dist/FrameworkWith_u8g2/production/lab4.production.hex
 CND_PACKAGE_DIR_FrameworkWith_u8g2=${CND_DISTDIR}/FrameworkWith_u8g2/package
-CND_PACKAGE_NAME_FrameworkWith_u8g2=framework4pic32withu8g2.tar
-CND_PACKAGE_PATH_FrameworkWith_u8g2=${CND_DISTDIR}/FrameworkWith_u8g2/package/framework4pic32withu8g2.tar
-=======
-#
-# Generated - do not edit!
-#
-# NOCDDL
-#
-CND_BASEDIR=`pwd`
-# Justu8g2POC configuration
-CND_ARTIFACT_DIR_Justu8g2POC=dist/Justu8g2POC/production
-CND_ARTIFACT_NAME_Justu8g2POC=lab4_latest.production.hex
-CND_ARTIFACT_PATH_Justu8g2POC=dist/Justu8g2POC/production/lab4_latest.production.hex
-CND_PACKAGE_DIR_Justu8g2POC=${CND_DISTDIR}/Justu8g2POC/package
-CND_PACKAGE_NAME_Justu8g2POC=lab4latest.tar
-CND_PACKAGE_PATH_Justu8g2POC=${CND_DISTDIR}/Justu8g2POC/package/lab4latest.tar
-# FrameworkWith_u8g2 configuration
-CND_ARTIFACT_DIR_FrameworkWith_u8g2=dist/FrameworkWith_u8g2/production
-CND_ARTIFACT_NAME_FrameworkWith_u8g2=lab4_latest.production.hex
-CND_ARTIFACT_PATH_FrameworkWith_u8g2=dist/FrameworkWith_u8g2/production/lab4_latest.production.hex
-CND_PACKAGE_DIR_FrameworkWith_u8g2=${CND_DISTDIR}/FrameworkWith_u8g2/package
-CND_PACKAGE_NAME_FrameworkWith_u8g2=lab4latest.tar
-CND_PACKAGE_PATH_FrameworkWith_u8g2=${CND_DISTDIR}/FrameworkWith_u8g2/package/lab4latest.tar
->>>>>>> b2fa3dbdec436236a9b144d4fcbafd7dcb56f32d
+CND_PACKAGE_NAME_FrameworkWith_u8g2=lab4.tar
+CND_PACKAGE_PATH_FrameworkWith_u8g2=${CND_DISTDIR}/FrameworkWith_u8g2/package/lab4.tar
